a4e9929949849ade7907392acc042646a40d9b42
Simon Revill <midlandmusic@googlemail.com>
2026-03-28 23:15:16 +0000

    feat: add tests for contrast check failure

--- crates/primitiv-core/src/audit/tests/audit_tests.rs
+++ crates/primitiv-core/src/audit/tests/audit_tests.rs
@@ -1,22 +1,43 @@
 use super::super::audit::*;
 
 #[test]
-fn test_contrast_result_ratio() {
+fn test_contrast_result_ratio_success() {
     let result = get_contrast_rating("#000000", "#FFFFFF");
 
     assert_eq!(result.ratio, 21.0);
 }
 
 #[test]
-fn test_contrast_result_display_ratio() {
+fn test_contrast_result_display_ratio_success() {
     let result = get_contrast_rating("#000000", "#FFFFFF");
 
     assert_eq!(result.display_ratio, "21.00:1");
 }
 
 #[test]
-fn test_contrast_result_rating() {
+fn test_contrast_result_rating_success() {
     let result = get_contrast_rating("#000000", "#FFFFFF");
 
     assert_eq!(result.rating, "AAA");
+}
+
+#[test]
+fn test_contrast_result_ratio_fail() {
+    let result = get_contrast_rating("#FFC0CB", "#FFFFFF");
+
+    assert_eq!(result.ratio, 1.54);
+}
+
+#[test]
+fn test_contrast_result_display_ratio_fail() {
+    let result = get_contrast_rating("#FFC0CB", "#FFFFFF");
+
+    assert_eq!(result.display_ratio, "1.54:1");
+}
+
+#[test]
+fn test_contrast_result_rating_fail() {
+    let result = get_contrast_rating("#FFC0CB", "#FFFFFF");
+
+    assert_eq!(result.rating, "Fail");
 }

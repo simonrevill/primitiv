5487c7838ea0564fc313807828f878279d644cf4
Simon Revill <midlandmusic@googlemail.com>
2026-03-28 23:23:13 +0000

    feat: added tests for contrast audit with white text on black background

--- crates/primitiv-core/src/audit/tests/audit_tests.rs
+++ crates/primitiv-core/src/audit/tests/audit_tests.rs
@@ -22,6 +22,27 @@ fn test_contrast_result_rating_success() {
 }
 
 #[test]
+fn test_contrast_result_ratio_success_opposite() {
+    let result = get_contrast_rating("#FFFFFF", "#000000");
+
+    assert_eq!(result.ratio, 21.0);
+}
+
+#[test]
+fn test_contrast_result_display_ratio_success_opposite() {
+    let result = get_contrast_rating("#FFFFFF", "#000000");
+
+    assert_eq!(result.display_ratio, "21.00:1");
+}
+
+#[test]
+fn test_contrast_result_rating_success_opposite() {
+    let result = get_contrast_rating("#FFFFFF", "#000000");
+
+    assert_eq!(result.rating, "AAA");
+}
+
+#[test]
 fn test_contrast_result_ratio_fail() {
     let result = get_contrast_rating("#FFC0CB", "#FFFFFF");
 
@@ -40,4 +61,25 @@ fn test_contrast_result_rating_fail() {
     let result = get_contrast_rating("#FFC0CB", "#FFFFFF");
 
     assert_eq!(result.rating, "Fail");
+}
+
+#[test]
+fn test_contrast_result_ratio_double_a() {
+    let result = get_contrast_rating("#767676", "#FFFFFF");
+
+    assert_eq!(result.ratio, 4.54);
+}
+
+#[test]
+fn test_contrast_result_display_ratio_double_a() {
+    let result = get_contrast_rating("#767676", "#FFFFFF");
+
+    assert_eq!(result.display_ratio, "4.54:1");
+}
+
+#[test]
+fn test_contrast_result_rating_double_a() {
+    let result = get_contrast_rating("#767676", "#FFFFFF");
+
+    assert_eq!(result.rating, "AA");
 }

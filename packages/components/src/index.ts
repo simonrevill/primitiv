4c79c5c71be1934adc5aebcd1c661044843ab7fa
simonrevill <midlandmusic@googlemail.com>
2026-04-20 22:29:03 +0100

    feat(index): export Dropdown component from index file

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./Accordion";
 export * from "./Checkbox";
 export * from "./Divider";
+export * from "./Dropdown";
 export * from "./Modal";
 export * from "./RadioGroup";
 export * from "./Table";

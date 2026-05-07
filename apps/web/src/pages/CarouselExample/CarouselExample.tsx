210d3317d6369d0daff4bccf098e6fbab3b810d1
simonrevill <midlandmusic@googlemail.com>
2026-05-07 17:05:09 +0100

    feat(carousel): enhance CarouselExample with new image slides and improved controls layout

--- apps/web/src/pages/CarouselExample/CarouselExample.tsx
+++ apps/web/src/pages/CarouselExample/CarouselExample.tsx
@@ -4,33 +4,41 @@ import "./CarouselExample.scss";
 
 export function CarouselExample() {
   return (
-    <Carousel.Root
-      className="carousel"
-      ariaLabel="Featured products"
-      slidesPerPage={3}
-      loop
-    >
+    <Carousel.Root className="carousel" ariaLabel="Featured products" loop>
       <Carousel.Viewport className="carousel__viewport">
-        {[1, 2, 3, 4, 5, 6].map((item, index) => (
-          <Carousel.Slide key={item} className="carousel__slide">
+        {[
+          { src: "/cube.png" },
+          { src: "/sphere.png" },
+          { src: "/cylinder.png" },
+          { src: "/cone.png" },
+          { src: "/torus.png" },
+          { src: "/wedge.png" },
+          { src: "/pyramid.png" },
+        ].map(({ src }) => (
+          <Carousel.Slide key={src} className="carousel__slide">
             <img
               className="carousel__image"
-              src={`https://placehold.co/600x400@2x.png/?text=Slide+${index + 1}`}
+              src={src}
               alt="600 x 400 placeholder image"
             />
           </Carousel.Slide>
         ))}
       </Carousel.Viewport>
-      <Carousel.PreviousTrigger
-        className="carousel__trigger"
-        aria-label="Previous"
-      >
-        {"<"}
-      </Carousel.PreviousTrigger>
-      <Carousel.Indicators label="Choose slide" />
-      <Carousel.NextTrigger className="carousel__trigger" aria-label="Next">
-        {">"}
-      </Carousel.NextTrigger>
+      <div className="carousel__controls">
+        <Carousel.PreviousTrigger
+          className="carousel__trigger"
+          aria-label="Previous"
+        >
+          {"<"}
+        </Carousel.PreviousTrigger>
+        <Carousel.Indicators
+          label="Choose slide"
+          className="carousel__indicator-group"
+        />
+        <Carousel.NextTrigger className="carousel__trigger" aria-label="Next">
+          {">"}
+        </Carousel.NextTrigger>
+      </div>
     </Carousel.Root>
   );
 }
